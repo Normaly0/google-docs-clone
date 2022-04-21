78b089bb2b6fb84830aa577a141abdadf38ad5e9
Normaly <40768391+Normaly0@users.noreply.github.com>
2022-04-21 19:18:27 +0200

    Added Sass

---
+++ client/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Navbar;
